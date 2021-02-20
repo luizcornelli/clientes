f920515cc2c6ecca67da8869a999adc1eac166ca
Luiz Cornelli <luizcornelli.dev@gmail.com>
2021-02-20 05:03:47 -0300

    Entity Cliente and Lombok

---
+++ backend/src/main/java/com/luizcornelli/model/Cliente.java
@@ -0,0 +1,29 @@
+package com.luizcornelli.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Cliente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(nullable = false, length = 150)
+	private String nome;
+
+	@Column(nullable = false, length = 11)
+	private String cpf;
+
+	@Column
+	private LocalDate dataCadastro;
+}
