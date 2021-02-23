7f3e3dde80c2614f467db806dc64be854a385fbd
Luiz Cornelli <luizcornelli.dev@gmail.com>
2021-02-23 18:45:57 -0300

    Method of ServicosPrestados

--- backend/src/main/java/com/luizcornelli/repository/ServicoPrestadoRepository.java
+++ backend/src/main/java/com/luizcornelli/repository/ServicoPrestadoRepository.java
@@ -1,9 +1,17 @@
 package com.luizcornelli.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.luizcornelli.model.ServicoPrestado;
 
 public interface ServicoPrestadoRepository extends JpaRepository<ServicoPrestado, Integer>{
+
+	@Query("select s from ServicoPrestado s join s.cliente c "
+			+ " where upper(c.nome) like upper(:nome) and MONTH(s.data) = :mes")
+	List<ServicoPrestado> findByNomeAndMes(@Param("nome") String nome, @Param("mes") Integer mes);
 
 }
